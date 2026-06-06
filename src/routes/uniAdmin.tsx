2004140de533d16eb05b0b2e85e0bf1180ab67d2
aviTD27 <avi.doorga27@gmail.com>
2026-06-06 14:20:42 +0200

    Feat: ACL-F002 - Dashboard Skeleton

--- src/routes/uniAdmin.tsx
+++ src/routes/uniAdmin.tsx
@@ -1,20 +1,26 @@
 import { Route } from 'react-router-dom'
-// import Layout from '../shared/layout/Layout'
+import Layout from '../shared/layout/Layout'
 import UniAdminDashboard from '../uni-admin/pages/UniAdminDashboard'
 
-// function Page({ title }: { title: string }) {
-//   return (
-//     <Layout>
-//       <h1 className="page-title">{title}</h1>
-//       <p className="page-subtitle">Page content</p>
-//     </Layout>
-//   )
-// }
+function Page({ title }: { title: string }) {
+  return (
+    <Layout>
+      <h1 className="page-title">{title}</h1>
+      <p className="page-subtitle">Page content</p>
+    </Layout>
+  )
+}
 
 export default function UniAdminRoutes() {
   return (
     <>
-      <Route path="/uni-admin" element={<UniAdminDashboard />} />
+      <Route path="/uni-admin" element={<UniAdminDashboard/>} />
+      <Route path="/uni-admin/cohorts" element={<Page title="Cohort Management" />} />
+      <Route path="/uni-admin/lecturers" element={<Page title="Lecturers" />} />
+      <Route path="/uni-admin/students" element={<Page title="Students" />} />
+      <Route path="/uni-admin/compliance" element={<Page title="Compliance" />} />
+      <Route path="/uni-admin/analytics" element={<Page title="Analytics" />} />
+      <Route path="/uni-admin/settings" element={<Page title="Settings" />} />
     </>
   )
 }
