ebd6e3fc4eb05467d1d96c1f76cde96b32dcc871
aviTD27 <avi.doorga27@gmail.com>
2026-07-07 13:30:50 +0200

    Feat: Fix Endpoints Details

--- src/api/registrations.ts
+++ src/api/registrations.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import apiClient from './apiClient'
 
 const publicApi = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8000/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
